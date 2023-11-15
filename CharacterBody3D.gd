d35b4696c6b4c68f615ad0ef1b6061beeb31f729
Brian Vose <BRVose@gmail.com>
2023-11-14 20:40:09 -0700

    added weapon models

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -15,11 +15,14 @@ var gravity = 10.5
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
-func _unhandled_input(event):
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+		camera.rotation.x = clamp(camera.rotation.x, -1.5, 1.5)
 
 func _physics_process(delta):
 	# Add the gravity.
