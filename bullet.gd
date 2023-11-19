417da41294f05e6d36c265bfe741ef8560677621
Brian Vose <BRVose@gmail.com>
2023-11-19 15:36:19 -0700

    added clumsy hit indicator for proj bullet

--- bullet.gd
+++ bullet.gd
@@ -1,10 +1,14 @@
 extends RigidBody3D
 
 var Damage: int = 0
-
+var Debug_bullet = preload("res://bullet_debug.tscn")
 
 
 func _on_body_entered(body):
+	var Hit_indicator = Debug_bullet.instantiate()
+	var world = get_tree().get_root().get_child(0)
+	world.add_child(Hit_indicator)
+	Hit_indicator.global_translate(body.position)
 	if body.is_in_group("Target") and body.has_method("Hit_sucessful"):
 		body.Hit_sucessful(Damage)
 		queue_free()
