0d47d8583a1804a249471ef79dc2ce7354b4c9f5
Brian Vose <BRVose@gmail.com>
2023-11-24 18:26:25 -0700

    added crouching

--- scripts/CharacterBody3D.gd
+++ scripts/CharacterBody3D.gd
@@ -1,14 +1,17 @@
 extends CharacterBody3D
 
 
-const SPEED = 7.0
+const NORM_SPEED = 7.0
 const JUMP_VELOCITY = 5.5
 const SENSITIVITY = 0.003
+const CROUCH_SPEED = 4.5
 
+var speed = NORM_SPEED
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 10.5
-
-
+@onready var collision_stand = $CollisionStand
+@onready var collision_crouch = $CollisionCrouch
+@onready var ray_cast_crouch = $RayCastCrouch
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
 
@@ -35,14 +38,24 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	if Input.is_action_pressed("crouch") && is_on_floor():
+		collision_stand.disabled = true
+		collision_crouch.disabled = false
+		head.position.y = lerp(head.position.y, .75, delta * 10)
+		speed = CROUCH_SPEED
+	elif !ray_cast_crouch.is_colliding():
+		collision_stand.disabled = false
+		collision_crouch.disabled = true
+		head.position.y = lerp(head.position.y, 1.5, delta * 10)
+		speed = NORM_SPEED
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
 		velocity.x = 0.0
 		velocity.z = 0.0
