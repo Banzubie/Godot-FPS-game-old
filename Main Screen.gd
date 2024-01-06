c580859b04b4cde5b0ade0d3b5b7fe35f3ee1655
Brian Vose <BRVose@gmail.com>
2024-01-06 12:51:40 -0700

    added start of forest map

--- Main Screen.gd
+++ Main Screen.gd
@@ -8,7 +8,7 @@ func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 func _on_bridge_pressed():
-	get_tree().change_scene_to_file("res://scenes/bridge.tscn")
+	get_tree().change_scene_to_file("res://scenes/forest.tscn")
 
 
 
